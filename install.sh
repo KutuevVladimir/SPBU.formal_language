319b8dfb3aa236762bbdce4cf032cfd4414a3fd7
KutuevVladimir <VladimirKutuev@gmail.com>
2018-12-23 03:58:05 +0300

    added openmp

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo "----- Install dependencies -----"
 sudo apt-get update
 sudo apt-get install -y dh-autoreconf wget
+sudo apt-get install libomp-dev
 
 echo "----- CMake -----"
 wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2-Linux-x86_64.tar.gz
@@ -14,7 +15,7 @@ git clone https://vkutuev@bitbucket.org/vkutuev/m4ri.git
 cd m4ri/
 autoreconf --install
 autoreconf --install
-./configure
+./configure --enable-openmp
 sudo make install
 echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"' >> $BASH_ENV
 cd ..
